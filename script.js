82b79d35c83f778296736d46e3065bc1d119c035
pukpuk555 <jester2540@gmail.com>
2024-05-14 11:13:59 +0700

    complete

---
+++ script.js
@@ -0,0 +1,27 @@
+let dayNumber = 7;
+
+switch (true) {
+  case dayNumber === 0:
+    console.log("Sunday");
+    break;
+  case dayNumber === 1:
+    console.log("Monday");
+    break;
+  case dayNumber === 2:
+    console.log("Tuesday");
+    break;
+  case dayNumber === 3:
+    console.log("Wednesday");
+    break;
+  case dayNumber === 4:
+    console.log("Thursday");
+    break;
+  case dayNumber === 5:
+    console.log("Friday");
+    break;
+  case dayNumber === 6:
+    console.log("Saturday");
+    break;
+  default:
+    console.log("Invalid day number");
+}
